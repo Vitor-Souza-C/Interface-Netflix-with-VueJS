ef763823600680e3c1edc0ffa46258a19a9d9ed7
Vitor Souza <vitor.souzac@hotmail.com>
2022-09-01 23:15:31 -0300

    (feat/adjustment-footer-innitial) added container component for social media and creator.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,23 +2,42 @@
     <div>
         <div class="info-content">
             <FooterModel v-slot="{hidden, message, languages, infoPopulate}"> <!--info/contact-->
-                <div>
-                    <h4 id="socialMidia">Redes Sociais</h4>
-                    <ul v-for="social in infoPopulate.socialMidia">
-                        <li><b>{{social.id}}:</b> {{social.info}}</li>
-                    </ul>
-                </div>
-                <div class="footer-container"> 
-                    <p>{{message + infoPopulate.creator}}</p>
-                    <p>{{infoPopulate.date}} - {{infoPopulate.region}}</p>
-                </div> 
-                <div> <!-- images --></div> 
+                <b-container class="bv-example-row">
+                    <b-row>
+                        <b-col class=" pt-3">
+                            <span class="mr-3">
+                                <b-button variant="primary" id  ="socialInstagram" class="mb-2 " title="vitorcarvalho/twitter.com">
+                                    <b-icon-instagram /> 
+                                </b-button>
+                            </span>
+                            <span class="mr-3">
+                                <b-button variant="primary" id="socialTwitter" class="mb-2" title="vitorcarvalho/instagram.com.br">
+                                    <b-icon-twitter />
+                                </b-button>
+                            </span>
+                            <span>
+                                <b-button variant="primary" id="socialFacebook"  class="mb-2" title="vitorcarvalho/facebook.com.br"> 
+                                    <b-icon-facebook />
+                                </b-button>
+                            </span>
+                        </b-col>
+                        <b-col>
+                            <b-card bg-variant="danger" text-variant="white" class="text-center font-family">
+                                <b-card-text>
+                                    <span>{{message + infoPopulate.creator}}</span> 
+                                </b-card-text>
+                            </b-card>
+                        </b-col>
+                    </b-row>
+                </b-container>
             </FooterModel>
         </div>
     </div>
 </template>
 
 <script>
+
+// {id: 'facebook', info: ''}, {id: 'twitter', info: ''}, {id: 'instagram', info: ''}
 
 import FooterModel from '../models/footer/FooterModel.vue'
 
@@ -35,15 +54,30 @@ export default {
     text-align: center;
 }
 .info-content{
-    padding-top: 100px;
-    padding-bottom: 80px;
     color: white;
-    background-color: black;
+    background: rgb(37,29,26);
+    background: linear-gradient(90deg, rgba(37,29,26,1) 0%, rgba(92,30,10,1) 35%, rgba(60,27,27,1) 100%);
+    padding-bottom: 3vh;
 }
 .container{
-    padding-top: 5rem;
+    padding-top: 2rem;
 }
 #socialMidia{
     padding-left: 1.2rem;
+}
+#socialInstagram{
+    border-color: #bc318f;
+    background: radial-gradient(circle farthest-corner at 35% 90%, #fec564, transparent 50%), radial-gradient(circle farthest-corner at 0 140%, #fec564, transparent 50%), radial-gradient(ellipse farthest-corner at 0 -25%, #5258cf, transparent 50%), radial-gradient(ellipse farthest-corner at 20% -50%, #5258cf, transparent 50%), radial-gradient(ellipse farthest-corner at 100% 0, #893dc2, transparent 50%), radial-gradient(ellipse farthest-corner at 60% -20%, #893dc2, transparent 50%), radial-gradient(ellipse farthest-corner at 100% 100%, #d9317a, transparent), linear-gradient(#6559ca, #bc318f 30%, #e33f5f 50%, #f77638 70%, #fec66d 100%);
+}
+#socialTwitter{
+    border-color:#00aced;
+    background: #00aced
+}
+#socialFacebook{
+    border-color:#3b5998;
+    background: #3b5998;
+}
+.font-family{
+    font-family: monospace;
 }
 </style>
